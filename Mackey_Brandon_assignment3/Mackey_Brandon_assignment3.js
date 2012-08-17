929d31fa7abf4419af2ab23fa2076b3c628bdf66
Brandon Mackey <brandonmackey@fullsail.edu>
2012-08-17 18:32:32 -0400

    Added Global Variable and Boolean functions

---
+++ Mackey_Brandon_assignment3/Mackey_Brandon_assignment3.js
@@ -0,0 +1,32 @@
+//alert("JavaScript works!");
+
+// Brandon Mackey	
+// August 17, 2012
+// SDI Wk 3 Project 3
+// 1208 
+// Story making own flowchart
+
+var phone = "Samsung Galaxy S3";
+var face = "face recognition";
+var app = "ichat";
+var arrFriend = ["Lee", "Jen", "Chad"];
+var phLock = 3;
+var unlocked = true;
+
+// boolean 
+
+function checkFace(xxxx) {
+        if (unlocked === false) {
+            console.log("Phone unlocked, Welcome to the " + phone + "!");
+        } else {
+            console.log ("Error! poor lighting. Enter " + xxxx + ".");
+        }
+        return checkFace    
+};
+
+
+
+
+
+
+
