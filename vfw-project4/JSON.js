1332e07ebf9a245ffbe2c86e3dee571b59376af3
Brandon Mackey <brandonmackey@fullsail.edu>
2012-09-20 13:57:52 -0400

    Added JSON Dummy Data and Icons for my different groups

---
+++ vfw-project4/JSON.js
@@ -0,0 +1,41 @@
+/*
+Brandon Mackey
+Web App Part 4
+Term 1209
+Sept 16, 2012
+VFW JSON Object for Testing
+*/
+
+var json = {
+    "list1":{
+	"group"	    : ["Type:", "Don't Forget!"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-07-17"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "BO2"],
+	"where"     : ["Where:", "Gamestop"],
+	"notes"     : ["Notes:", "Black Ops 2"],
+	"favorite"  : ["Favorite:", "Yes"]
+    },
+    "list2":{
+	"group"	    : ["Type:", "Honey Do List!"],
+	"importance": ["Priority:", "Low Priority" ],
+	"date"	    : ["Date:", "2012-09-25"],
+	"quantity"  : ["Quantity:", "0"],
+	"what"      : ["What:", "Mow"],
+	"where"     : ["Where:", "My House"],
+	"notes"     : ["Notes:", "Because she said so!"],
+	"favorite"  : ["Favorite:", "No"]
+    },
+    "list3":{
+	"group"	    : ["Type:", "Grocery List"],
+	"importance": ["Priority:", "High Priority" ],
+	"date"	    : ["Date:", "2012-09-16"],
+	"quantity"  : ["Quantity:", "1"],
+	"what"      : ["What:", "Baby Dipers"],
+	"where"     : ["Where:", "Wal-Mart"],
+	"notes"     : ["Notes:", "Size 3"],
+	"favorite"  : ["Favorite:", "Yes"]
+    }
+}
+
