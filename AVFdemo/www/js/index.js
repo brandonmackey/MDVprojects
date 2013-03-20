28b9063439fefbba3c5b5ee1609d169c5fef1e18
Brandon Mackey <brandonmackey@fullsail.edu>
2013-03-20 18:18:36 -0400

    update need help the popup button and bleeding pages

--- AVFdemo/www/js/index.js
+++ AVFdemo/www/js/index.js
@@ -1,44 +1,40 @@
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
 $('#home').on('pageinit', function(){
-
+    var app = {
+        // Application Constructor
+        initialize: function() {
+            this.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+        // deviceready Event Handler
+        //
+        // The scope of 'this' is the event. In order to call the 'receivedEvent'
+        // function, we must explicity call 'app.receivedEvent(...);'
+        onDeviceReady: function() {
+            app.receivedEvent('deviceready');
+        },
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+    
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+    
+            console.log('Received Event: ' + id);
+        }
+    };
 });
-
 
     // ------  Twitter DATA  -------//
 var getData = function() {
-	$.getJSON('http://search.twitter.com/search.json?q=samsung&mobile=15include_entities=true&callback=?',
+	$.getJSON('http://search.twitter.com/search.json?q=samsung&galaxy=15include_entities=result_type=true&callback=?',
             function(data) {
             console.log(data);
             for (i=0, j=data.results.length; i<j; i++) {
@@ -61,4 +57,40 @@ var getData = function() {
 
 $('#getData').on('click', function() {
     getData();
+});
+    //------ MAPS / GEOLOCATION -----//
+
+
+$( document ).on( "pageinit", function() {
+    $( "#geoMap iframe" )
+        .attr( "width", 620 )
+        .attr( "height", 480 );
+        
+    $( "#geoMap iframe" ).contents().find( "#mapScreen" )
+        .css( { "width" : 0, "height" : 0 } );
+
+    $( "#geoMap" ).on({
+        popupbeforeposition: function() {
+            var size = scale( 480, 320, 0, 1 ),
+                w = size.width,
+                h = size.height;
+
+            $( "#geoMap iframe" )
+                .attr( "width", w )
+                .attr( "height", h );
+                
+            $( "#geoMap iframe" ).contents().find( "#mapScreen" )
+                .css( { "width": w, "height" : h } );
+                
+        },
+        popupafterclose: function() {
+            $( "#geoMap iframe" )
+                .attr( "width", 0 )
+                .attr( "height", 0 );
+
+            $( "#geoMap iframe" ).contents().find( "#mapScreen" )
+                .css( { "width": 0, "height" : 0 } );
+                location.reload();
+        }
+    });
 });
